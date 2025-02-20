58c5c23a56f0a2588e596926669d2d79c8963321
Benfranklin m s <bfranklinms@gmail.com>
2025-02-20 23:26:55 +0530

    Add winner announcement and new game functionality to Tic Tac Toe

--- Tic tac toe/script.js
+++ Tic tac toe/script.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
 
 let turnO = true;
 const winPatterns = [
@@ -18,21 +22,76 @@ boxes.forEach((box) => {
         console.log("box was clicked");
         if(turnO){
             box.innerText = "O";
+            box.style.color = "#A8DCD9";
             turnO = false;
         }
         else{
             box.innerText = "X";
+            box.style.color = "#E2A3C7";
             turnO = true;
         }
         box.disabled = true;
 
         checkWinner();
+        checkDraw();
     });
 });
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+}
+
+const enableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = false;
+    });
+}
+const showWinner = (winner) => {
+    msg.innerText = `${winner} wins!`;
+    msgContainer.classList.remove("hide");
+}
 
 const checkWinner = () => {
     winPatterns.forEach((pattern) => {
-        console.log(pattern);
-        
-    })
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val == pos2Val && pos2Val == pos3Val){
+                showWinner(pos1Val);
+                    boxes[pattern[0]].classList.add("winColor");
+                    boxes[pattern[1]].classList.add("winColor");
+                    boxes[pattern[2]].classList.add("winColor");
+                disableBoxes();
+            }
+        }
+    });
+};
+
+const checkDraw = () => {
+    let isDraw = true;
+    boxes.forEach((box) => {
+        if(box.innerText == ""){
+            isDraw = false;
+        }
+    });
+    if(isDraw){
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+    }
 }
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.classList.remove("winColor");
+    });
+    msgContainer.classList.add("hide");
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
