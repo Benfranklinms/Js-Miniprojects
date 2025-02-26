b374841c99d2e9844ffd401257040727ee0ac297
Benfranklin m s <bfranklinms@gmail.com>
2025-02-26 11:50:23 +0530

    Fix addTask function to properly handle empty input and create list items

--- To do list basic/app.js
+++ To do list basic/app.js
@@ -2,8 +2,13 @@ const input = document.querySelector('#input');
 const add = document.querySelector('#add');
 const list = document.querySelector('#list-container');
 
-addTask = () => {
-    if(input === ""){
+const addTask = () => {
+    const text = input.value.trim();
+    if(text === ""){
         alert("Please input text");
+    }
+    else{
+        const li = document.createElement('li');
+        li.textContent(text)
     }
 }
