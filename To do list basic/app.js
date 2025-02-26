f3c96e9572832f444ea37c4b0fe4c1368c5e653d
Benfranklin m s <bfranklinms@gmail.com>
2025-02-26 11:45:24 +0530

    Add basic functionality and styling for To-Do List application

---
+++ To do list basic/app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#input');
+const add = document.querySelector('#add');
+const list = document.querySelector('#list-container');
+
+addTask = () => {
+    if(input === ""){
+        alert("Please input text");
+    }
+}
